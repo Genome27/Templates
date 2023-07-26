99f9afd00c7de5339bd74c3cca227d8663cb68b9
Genome27 <140137378+Genome27@users.noreply.github.com>
2023-07-26 12:37:21 +0530

    Update app_template.py
    Working Code, Does not refer to context.

--- OpenAI_Template/app_template.py
+++ OpenAI_Template/app_template.py
@@ -1,50 +1,21 @@
-#generic template for web based application using openai
-
 import openai
-from modules.substitute import *
-from flask import Flask, render_template, request
-
-openai.api_key = 'OPENAI KEY HERE'
-#Web framework component
-app=Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('mywebpage.html') #refer to your webpage htmp file here
-
-#get requests retrieve data from server
-#post request upload data to a server for processing and may alter the server state
 
 
-#@app.route("/get", methods=["GET", "POST"])
+openai.api_key = 'YOUR API KEY'
 
+delimiter = "####"
+prompt = f'''YOUR PROMPT HERE
+    '''
+messages = [{"role":"system", "content":prompt}]
 
-#to import another python file in the same directory, or mentioned directory use the syntax: 
-# import dir.filename(python file) import *(all) 
+def get_completion(prompt, model="gpt-3.5-turbo"):
 
+  #messages = [{"role":"system", "content":prompt}]
+  messages.append({"role":"user", "content":prompt_input},)
+  chat = openai.ChatCompletion.create(model=model, messages=messages, temperature=0)
+  response = chat.choices[0].message["content"]
+  return response
 
-#This list sets a context and user inputs are appended here.
-messages = []
-
-def main(user_message):
-    #main function to set prompt and context
-    
-    delimiter="####"
-    prompt='''Enter your prompt here:
-    
-    end of prompt'''
-
-    messages = [ {"role": "system", "content": f"{prompt}"}]
-    messages.append({"role":"user", "content":f"{delimiter}{user_message}{delimiter}"})
-    
-    chat= openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, temperature = 0)
-    
-    #return the message
-    return chat.choices[0].message.content
-
-
-#using flask for web based application
-
-if __name__ == "__main__":
-    app.run()
-    
+while True:
+  prompt_input = input("Enter a prompt to the AI:   ")
+  print(get_completion(prompt_input) )
